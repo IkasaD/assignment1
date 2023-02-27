724c7b3e160a6ef2e88709a1fd89ba31eb3f6f53
Mohammed Sarfraz Hussain <125431995+IkasaD@users.noreply.github.com>
2023-02-27 21:15:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
-
+import random
 
 st.title("Hi!!")
-st.header("Welcome to my Page")
-st.write("Want to see magic click the button")
-if st.button("button"):
+st.header("Welcome to my Data Visualization Page")
+st.write("Want to see magic click it")
+
+
+
+if st.button(f"Click Me👈 "):
     st.snow()
     st.balloons()
     st.write(":100:")
     st.write("Check the dashboards")
-    video_file = open('bear.mp4', 'rb')
-    video_bytes = video_file.read()
-    st.video(video_bytes)
+    st.write("Enjoy :sunglasses::alien:")
     
 
 
