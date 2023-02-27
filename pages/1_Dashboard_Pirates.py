d0d7e28244ee79d85ba5f5e700634b3228f15421
Mohammed Sarfraz Hussain <125431995+IkasaD@users.noreply.github.com>
2023-02-27 09:54:56 +0530

    Add files via upload

--- pages/1_Dashboard_Pirates.py
+++ pages/1_Dashboard_Pirates.py
@@ -13,7 +13,7 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 # absolute path to this file's root directory
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
 # absolute path of directory_of_interest
-dir_of_interest = os.path.join(PARENT_DIR, "resources")
+dir_of_interest = os.path.join(PARENT_DIR, "Resources")
 
 IMAGE_PATH = os.path.join(dir_of_interest, "images", "pir.png")
 
